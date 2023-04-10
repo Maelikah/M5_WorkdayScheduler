9351702afb64a8cb09a348cba15aa6385df600a3
Maelikah <eva.longoria@gmail.com>
2023-04-10 11:30:43 -0600

    Added jquery code to get current date into header

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+//Declare today's date variable
+var today = dayjs();
+// console log to see if desired format has been achieved
+console.log(today.format("dddd, MMMM D YYYY"));
+// Add today's date to the header
+$("#currentDay").text(today.format("dddd, MMMM D YYYY")); 
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
